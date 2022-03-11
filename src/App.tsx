cbafec38e313bec2f7e1604d8846c4a3818e6fee
Alisterj <lisarsiss@gmail.com>
2022-03-11 16:24:00 +0300

    filter lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+type FilterButtonsType = 'all' | 'RUBLS' | 'Dollars'
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [money, setMoney] = useState([
+        { banknots: 'Dollars', value: 100, number: ' a1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' z1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' w1234567890' },
+        { banknots: 'Dollars', value: 100, number: ' e1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' c1234567890' },
+        { banknots: 'RUBLS', value: 100, number: ' r1234567890' },
+        { banknots: 'Dollars', value: 50, number: ' x1234567890' },
+        { banknots: 'RUBLS', value: 50, number: ' v1234567890' },
+    ])
+
+    const [filter, setFilter] = useState<FilterButtonsType>('all')
+
+    const getFilterForMoney = () => {
+        switch (filter){
+            case "RUBLS":
+                return money.filter(f => f.banknots === 'RUBLS')
+            case "Dollars":
+                return money.filter(f => f.banknots === 'Dollars')
+            default:
+                return money
+        }
+    }
+
+    const currentMoney = getFilterForMoney().map(cash => {
+        return(
+            <li>
+                <span> {cash.banknots}</span>
+                <span> {cash.value}</span>
+                <span> {cash.number}</span>
+            </li>
+        );
+    })
+
+    const filterButton = (nameButton: FilterButtonsType) => {
+        setFilter(nameButton)
+    }
+
+    return (
+        <div>
+            <ul>
+                {currentMoney}
+            </ul>
+            <div className={'filterButtons'}>
+                <button onClick={() => filterButton('all')}>all</button>
+                <button onClick={() => filterButton('RUBLS')}>rubles</button>
+                <button onClick={() => filterButton('Dollars')}>Dollars</button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
